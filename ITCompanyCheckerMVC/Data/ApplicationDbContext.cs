3c31018eab2ff5c853444a5699e6b379f8613943
Lyub4i <87826275+Lyub4i@users.noreply.github.com>
2022-07-01 17:34:56 +0300

    Correct ApplicationDbContext

--- ITCompanyCheckerMVC/Data/ApplicationDbContext.cs
+++ ITCompanyCheckerMVC/Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ITCompanyCheckerMVC.Areas.Identity.Data;
 
@@ -18,5 +19,19 @@ public class ApplicationDbContext : IdentityDbContext<Employee>
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+}
+
+public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<Employee>
+{
+    public void Configure(EntityTypeBuilder<Employee> builder)
+    {
+        builder.Property(x => x.FirstName).HasMaxLength(255);
+        builder.Property(x => x.LastName).HasMaxLength(255);
+        builder.Property(x => x.Login).HasMaxLength(255);
+        builder.Property(x => x.Status).HasMaxLength(255);
+        builder.Property(x => x.LastUpdate);
+        builder.Property(x => x.Hours);
     }
 }
--- ITCompanyCheckerMVC/Models/Employee.cs
+++ ITCompanyCheckerMVC/Models/Employee.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -9,6 +11,19 @@ namespace ITCompanyCheckerMVC.Models;
 // Add profile data for application users by adding properties to the Employee class
 public class Employee : IdentityUser
 {
-    
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [Column(TypeName="nvarchar(20)")]
+    public string FirstName { get; set; }
+    [Required]
+    [Column(TypeName = "nvarchar(20)")]
+    public string LastName { get; set; }
+    [Column(TypeName = "nvarchar(20)")]
+    public string Login { get; set; }
+    public DateTime LastUpdate { get; set; }
+    public int Hours { get; set; }
+    [Column(TypeName = "nvarchar(15)")]
+    public string Status { get; set; }
 }
 
