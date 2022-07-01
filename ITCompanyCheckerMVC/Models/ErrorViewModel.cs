be3da6b9efb30bf115b8d52aa0fdddac47a3aee8
Lyub4i <87826275+Lyub4i@users.noreply.github.com>
2022-07-01 14:16:43 +0300

    Add project files.

---
+++ ITCompanyCheckerMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ITCompanyCheckerMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
