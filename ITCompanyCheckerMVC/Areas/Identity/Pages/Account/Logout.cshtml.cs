ba4913e5c39ebc2d80c50e8d721235581d608963
Lyub4i <87826275+Lyub4i@users.noreply.github.com>
2022-07-05 14:32:07 +0300

    redirect to Index Page

--- ITCompanyCheckerMVC/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ ITCompanyCheckerMVC/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -24,13 +24,13 @@ namespace ITCompanyCheckerMVC.Areas.Identity.Pages.Account
             _logger = logger;
         }
 
-        public async Task<IActionResult> OnPost(string returnUrl = null)
+        public async Task<IActionResult> OnPost(string returnUrl = "Main/Index")
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
-                return LocalRedirect(returnUrl);
+                return RedirectToPage("Index");
             }
             else
             {
